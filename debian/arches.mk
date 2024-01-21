ee164166c8105d8dcdfcea2cd0344257ace527d1
Andrea Bolognani <eof@kiyuko.org>
2024-01-21 16:10:15 +0100

    rules: Split out arches
    We will soon start using this list of architectures not just
    as part of debian/rules, but also as an input for other
    tooling.
    
    Gbp-Dch: Ignore

---
+++ debian/arches.mk
@@ -0,0 +1,9 @@
+ARCHES_CEPH = amd64 arm64 mips64el ppc64el riscv64 s390x
+ARCHES_GLUSTER = amd64 arm64 ia64 mips64el ppc64 ppc64el riscv64 s390x sparc64
+ARCHES_QEMU = amd64 arm64 armel armhf i386 mips64el mipsel powerpc ppc64 ppc64el riscv64 s390x sparc64 x32
+ARCHES_LXC = alpha amd64 arm64 armel armhf hppa i386 m68k mips64el mipsel powerpc ppc64 ppc64el riscv64 s390x sh4 sparc64 x32
+ARCHES_XEN = amd64 arm64 armhf
+ARCHES_VBOX = amd64 i386
+
+# Keep this list empty until we have AppArmor support
+ARCHES_NBDKIT =
